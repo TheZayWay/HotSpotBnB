646af74f13bb2feec2642458a6ef7b49ae1c0a29
isaiah <isaiahcodesforgood@gmail.com>
2023-05-12 15:49:42 -0700

    new seeder file

--- backend/db/seeders/20230511204025-demo-user.js
+++ backend/db/seeders/20230511204025-demo-user.js
@@ -1,15 +1,15 @@
 'use strict';
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 let options = {};
 if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;
+  options.schema = process.env.SCHEMA;  // define your schema in options object
 }
-/** @type {import('sequelize-cli').Migration} */
+
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  up: async (queryInterface, Sequelize) => {
     options.tableName = 'Users';
-    await queryInterface.bulkInsert(options, [
+    return queryInterface.bulkInsert(options, [
       {
         email: 'Warriors2023@gmail.com',
         username: 'ILoveTheGSW',
@@ -34,11 +34,11 @@ module.exports = {
     ], {});
   },
 
-  async down (queryInterface, Sequelize) {
-   options.tableName = 'Users';
-   const Op = Sequelize.Op;
-   await queryInterface.bulkDelete(options, {
-    username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+  down: async (queryInterface, Sequelize) => {
+    options.tableName = 'Users';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      username: { [Op.in]: ['ILoveTheGSW','WinSomeGifts','JusticeLeagueFTW'] }
     }, {});
   }
 };

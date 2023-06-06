5148824d7863a7b4c33d1e689ccff6c30842f7f3
isaiah <isaiahcodesforgood@gmail.com>
2023-06-06 15:08:53 -0700

    changes

--- backend/db/seeders/20230515213018-spotImages.js
+++ backend/db/seeders/20230515213018-spotImages.js
@@ -20,47 +20,47 @@ module.exports = {
         url: "https://img.staticmb.com/mbcontent/images/uploads/2022/12/Most-Beautiful-House-in-the-World.jpg",
         preview: true
       },
-      {
-        spotId: 3,
-        url: "https://sometimes-homemade.com/wp-content/uploads/2016/02/exterior.png",
-        preview: true
-      },
-      ,
-      {
-        spotId: 4,
-        url: "https://images.unsplash.com/photo-1628744448840-55bdb2497bd4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmljZSUyMGhvdXNlfGVufDB8fDB8fHww&w=1000&q=80",
-        preview: true
-      },
-      ,
-      {
-        spotId: 5,
-        url: "https://insidecolumbia.net/wp-content/uploads/2019/11/Exterior-Photos-3-copy_featuredImage.jpg",
-        preview: true
-      },
-      ,
-      {
-        spotId: 6,
-        url: "https://media.istockphoto.com/id/147205632/photo/modern-home-with-swimming-pool.jpg?s=612x612&w=0&k=20&c=sxRQ398SxAjC4FrRombjl46oDGJVdy23T7i3RXO-mww=",
-        preview: true
-      },
-      ,
-      {
-        spotId: 7,
-        url: "https://practicalhomes.com.au/wp-content/uploads/2019/06/Practical-Homes-36SQV1.jpg",
-        preview: true
-      },
-      ,
-      {
-        spotId: 8,
-        url: "https://maisonsinhaus.fr/wp-content/uploads/2021/12/modular-house-nice-pool-1024x576.jpg",
-        preview: true
-      },
-      ,
-      {
-        spotId: 9,
-        url: "https://live.staticflickr.com/5034/7058664963_e579b757ae_b.jpg",
-        preview: true
-      }
+      // {
+      //   spotId: 3,
+      //   url: "https://sometimes-homemade.com/wp-content/uploads/2016/02/exterior.png",
+      //   preview: true
+      // },
+      // ,
+      // {
+      //   spotId: 4,
+      //   url: "https://images.unsplash.com/photo-1628744448840-55bdb2497bd4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmljZSUyMGhvdXNlfGVufDB8fDB8fHww&w=1000&q=80",
+      //   preview: true
+      // },
+      // ,
+      // {
+      //   spotId: 5,
+      //   url: "https://insidecolumbia.net/wp-content/uploads/2019/11/Exterior-Photos-3-copy_featuredImage.jpg",
+      //   preview: true
+      // },
+      // ,
+      // {
+      //   spotId: 6,
+      //   url: "https://media.istockphoto.com/id/147205632/photo/modern-home-with-swimming-pool.jpg?s=612x612&w=0&k=20&c=sxRQ398SxAjC4FrRombjl46oDGJVdy23T7i3RXO-mww=",
+      //   preview: true
+      // },
+      // ,
+      // {
+      //   spotId: 7,
+      //   url: "https://practicalhomes.com.au/wp-content/uploads/2019/06/Practical-Homes-36SQV1.jpg",
+      //   preview: true
+      // },
+      // ,
+      // {
+      //   spotId: 8,
+      //   url: "https://maisonsinhaus.fr/wp-content/uploads/2021/12/modular-house-nice-pool-1024x576.jpg",
+      //   preview: true
+      // },
+      // ,
+      // {
+      //   spotId: 9,
+      //   url: "https://live.staticflickr.com/5034/7058664963_e579b757ae_b.jpg",
+      //   preview: true
+      // }
     ])
   },
 

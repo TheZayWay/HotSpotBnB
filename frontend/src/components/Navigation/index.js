1421cf273fc02425a35828a9b52b0ad1e3018281
isaiah <isaiahcodesforgood@gmail.com>
2023-06-06 21:16:27 -0700

    create spot button

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,6 +1,6 @@
 // frontend/src/components/Navigation/index.js
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
@@ -8,7 +8,26 @@ import hotSpot from './Images/hot-spot-pic.jpeg'
 
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
-  
+  let session;
+
+  if (!sessionUser) {
+    session = (
+      <div>
+        <ProfileButton user={sessionUser} />
+      </div>
+    )
+  } else {
+    session = (
+      <div>
+        <div>
+          <Link to="/spots/new"><button className="new-spot-button">Create a New Spot</button></Link>
+        </div>
+        <div>
+          <ProfileButton user={sessionUser} />
+        </div>
+      </div>
+    )
+  }
   return (
     <ul className="header">
       <li className="home">
@@ -18,11 +37,7 @@ function Navigation({ isLoaded }){
           </NavLink>
           
       </li>
-      {isLoaded && (
-        <li>
-          <ProfileButton user={sessionUser} />
-        </li>
-      )}
+      {isLoaded && session}
     </ul>
   );
 }

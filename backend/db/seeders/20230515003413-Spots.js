ab4245fda7058b5ff276478945b79cd80ff17ab8
isaiah <isaiahcodesforgood@gmail.com>
2023-06-06 17:25:57 -0700

    seeders fixed

--- backend/db/seeders/20230515003413-Spots.js
+++ backend/db/seeders/20230515003413-Spots.js
@@ -12,7 +12,7 @@ module.exports = {
    await queryInterface.bulkInsert(options, [
     {
       ownerId: 1,
-      address: "123 25th ave",
+      address: "120 25th ave",
       city: "San Francisco",
       state: "California",
       country: "United States of America",
@@ -35,28 +35,88 @@ module.exports = {
       price: 300
     },
     {
-      ownerId: 2,
-      address: "123 Main St",
+      ownerId: 1,
+      address: "400 Lyell St",
       city: "San Francisco",
       state: "California",
       country: "United States of America",
-      lat: 50.765358,
+      lat: 50.7653258,
       lng: -102.4330327,
-      name: "The Modern Home",
-      description: "Relaxation purposes only.",
-      price: 300
+      name: "The Brick House",
+      description: "An underrated vacation home",
+      price: 250
     },
     {
       ownerId: 2,
-      address: "123 Main St",
+      address: "50 30th ave",
       city: "San Francisco",
       state: "California",
       country: "United States of America",
-      lat: 50.765358,
+      lat: 50.7615358,
+      lng: -102.413302327,
+      name: "Grand Mansion",
+      description: "The only place to stay for a stellar trip.",
+      price: 400
+    },
+    {
+      ownerId: 1,
+      address: "545 Clayton St",
+      city: "San Francisco",
+      state: "California",
+      country: "United States of America",
+      lat: 50.7651358,
+      lng: -12.4330327,
+      name: "The Home",
+      description: "Perfect for those looking for a homey vibe",
+      price: 310
+    },
+    {
+      ownerId: 1,
+      address: "433 Bushwick st",
+      city: "San Francisco",
+      state: "California",
+      country: "United States of America",
+      lat: 51.765358,
       lng: -102.4330327,
-      name: "The Modern Home",
-      description: "Relaxation purposes only.",
-      price: 300
+      name: "The Flat Top",
+      description: "Perfect for parties.",
+      price: 350
+    },
+    {
+      ownerId: 2,
+      address: "390 Pierce St",
+      city: "San Francisco",
+      state: "California",
+      country: "United States of America",
+      lat: 50.7653582,
+      lng: -102.43303227,
+      name: "Metropolis",
+      description: "Fits large groups of people.",
+      price: 500
+    },
+    {
+      ownerId: 1,
+      address: "300 Popper St",
+      city: "San Francisco",
+      state: "California",
+      country: "United States of America",
+      lat: 5.765358,
+      lng: 902.4330327,
+      name: "The Incognito",
+      description: "Secluded. One of one.",
+      price: 400
+    },
+    {
+      ownerId: 1,
+      address: "40 Beacon st",
+      city: "San Francisco",
+      state: "California",
+      country: "United States of America",
+      lat: 30.765358,
+      lng: 42.4330327,
+      name: "The Spot",
+      description: "Luxurious",
+      price: 450
     }
    ])
   },
--- backend/db/seeders/20230515212848-spotImages.js
+++ backend/db/seeders/20230515212848-spotImages.js
@@ -22,12 +22,37 @@ module.exports = {
       },
       {
         spotId: 3,
-        url: "https://img.staticmb.com/mbcontent/images/uploads/2022/12/Most-Beautiful-House-in-the-World.jpg",
+        url: "https://practicalhomes.com.au/wp-content/uploads/2019/06/Practical-Homes-36SQV1.jpg",
         preview: true
       },
       {
         spotId: 4,
-        url: "https://img.staticmb.com/mbcontent/images/uploads/2022/12/Most-Beautiful-House-in-the-World.jpg",
+        url: "https://media.istockphoto.com/id/1150545984/photo/upscale-modern-mansion-with-pool.jpg?s=612x612&w=0&k=20&c=JT7qSGgmlGfiNiqJE2jw6rYwRcYCj9KBs7i2Rmyyypo=",
+        preview: true
+      },
+      {
+        spotId: 5,
+        url: "https://sometimes-homemade.com/wp-content/uploads/2016/02/exterior.png",
+        preview: true
+      },
+      {
+        spotId: 6,
+        url: " https://images.unsplash.com/photo-1628744448840-55bdb2497bd4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmljZSUyMGhvdXNlfGVufDB8fDB8fHww&w=1000&q=80",
+        preview: true
+      },
+      {
+        spotId: 7,
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxXAqp00vigLN_ub0joYrElLDkz3OYk7bQLkkbzmSxwRqWNIjmydNbWO9JDD9LwXR71b4&usqp=CAU",
+        preview: true
+      },
+      {
+        spotId: 8,
+        url: "https://media.istockphoto.com/id/1281554848/photo/dream-home-luxury-house-success-suburban-house.jpg?s=170667a&w=0&k=20&c=3iucZzHFZO_6k4Xth1nrqvs6lJSANgliiMUwMFy2nwA=",
+        preview: true
+      },
+      {
+        spotId: 9,
+        url: "https://architecturebeast.com/wp-content/uploads/2014/09/Most_Beautiful_Houses_In_The_World_House_M_featured_on_architecture_beast_39.jpg",
         preview: true
       }
     ])

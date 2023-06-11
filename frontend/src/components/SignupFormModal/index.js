31c5d0c17cbc42820e7e7bf9a235a9cc6bfb519b
isaiah <isaiahcodesforgood@gmail.com>
2023-06-11 14:43:35 -0700

    changes

--- frontend/src/components/SignupFormModal/index.js
+++ frontend/src/components/SignupFormModal/index.js
@@ -1,3 +1,130 @@
+// import React, { useState } from "react";
+// import { useDispatch } from "react-redux";
+// import { useModal } from "../../context/Modal";
+// import * as sessionActions from "../../store/session";
+// import "./SignupForm.css";
+
+// function SignupFormModal() {
+//   const dispatch = useDispatch();
+//   const [email, setEmail] = useState("");
+//   const [username, setUsername] = useState("");
+//   const [firstName, setFirstName] = useState("");
+//   const [lastName, setLastName] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [confirmPassword, setConfirmPassword] = useState("");
+//   const [errors, setErrors] = useState({});
+//   const { closeModal } = useModal();
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     if (password === confirmPassword) {
+//       setErrors({});
+//       return dispatch(
+//         sessionActions.signup({
+//           email,
+//           username,
+//           firstName,
+//           lastName,
+//           password,
+//         })
+//       )
+//         .then(closeModal)
+//         .catch(async (res) => {
+//           const data = await res.json();
+//           if (data && data.errors) {
+//             setErrors(data.errors);
+//           }
+//         });
+//     }
+//     return setErrors({
+//       confirmPassword: "Confirm Password field must be the same as the Password field"
+//     });
+//   };
+
+//   return (
+//     <>
+//     <div className="entire-signup-modal">
+//     <h1 className="signup-title">Sign Up</h1>
+//       <form className="signup-form" onSubmit={handleSubmit}>
+//         <label> 
+//           <input
+//             type="text"
+//             value={email}
+//             onChange={(e) => setEmail(e.target.value)}
+//             required
+//             placeholder="Email"
+//             className="signup-input-field"
+//           />
+//         </label>
+//         {errors.email && <p>{errors.email}</p>}
+//         <label>  
+//           <input
+//             type="text"
+//             value={username}
+//             onChange={(e) => setUsername(e.target.value)}
+//             required
+//             placeholder="Username"
+//             className="signup-input-field"
+//           />
+//         </label>
+//         {errors.username && <p>{errors.username}</p>}
+//         <label>
+//           <input
+//             type="text"
+//             value={firstName}
+//             onChange={(e) => setFirstName(e.target.value)}
+//             required
+//             placeholder="First Name"
+//             className="signup-input-field"
+//           />
+//         </label>
+//         {errors.firstName && <p>{errors.firstName}</p>}
+//         <label>
+//           <input
+//             type="text"
+//             value={lastName}
+//             onChange={(e) => setLastName(e.target.value)}
+//             required
+//             placeholder="Last Name"
+//             className="signup-input-field"
+//           />
+//         </label>
+//         {errors.lastName && <p>{errors.lastName}</p>}
+//         <label>
+//           <input
+//             type="password"
+//             value={password}
+//             onChange={(e) => setPassword(e.target.value)}
+//             required
+//             placeholder="Password"
+//             className="signup-input-field"
+//           />
+//         </label>
+//         {errors.password && <p>{errors.password}</p>}
+//         <label>
+//           <input
+//             type="password"
+//             value={confirmPassword}
+//             onChange={(e) => setConfirmPassword(e.target.value)}
+//             required
+//             placeholder="Confirm Password"
+//             className="signup-input-field"
+//           />
+//         </label>
+//         {errors.confirmPassword && (
+//           <p>{errors.confirmPassword}</p>
+//         )}
+//         <button type="submit" className="signup-button">Sign Up</button>
+//       </form>
+//     </div>    
+      
+//     </>
+//   );
+// }
+
+// export default SignupFormModal;
+
+
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
@@ -6,14 +133,18 @@ import "./SignupForm.css";
 
 function SignupFormModal() {
   const dispatch = useDispatch();
+  const { closeModal } = useModal();
+  
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  
   const [errors, setErrors] = useState({});
-  const { closeModal } = useModal();
+  
+  const isInvalid = password.length < 6 || username.length < 4;
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -37,87 +168,86 @@ function SignupFormModal() {
         });
     }
     return setErrors({
-      confirmPassword: "Confirm Password field must be the same as the Password field"
+      confirmPassword: "Confirm Password field must be the same as the Password field",
     });
   };
 
   return (
     <>
-    <div className="entire-signup-modal">
-    <h1 className="signup-title">Sign Up</h1>
-      <form className="signup-form" onSubmit={handleSubmit}>
-        <label> 
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            placeholder="Email"
-            className="signup-input-field"
-          />
-        </label>
-        {errors.email && <p>{errors.email}</p>}
-        <label>  
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-            placeholder="Username"
-            className="signup-input-field"
-          />
-        </label>
-        {errors.username && <p>{errors.username}</p>}
-        <label>
-          <input
-            type="text"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            required
-            placeholder="First Name"
-            className="signup-input-field"
-          />
-        </label>
-        {errors.firstName && <p>{errors.firstName}</p>}
-        <label>
-          <input
-            type="text"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            required
-            placeholder="Last Name"
-            className="signup-input-field"
-          />
-        </label>
-        {errors.lastName && <p>{errors.lastName}</p>}
-        <label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            placeholder="Password"
-            className="signup-input-field"
-          />
-        </label>
-        {errors.password && <p>{errors.password}</p>}
-        <label>
-          <input
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-            placeholder="Confirm Password"
-            className="signup-input-field"
-          />
-        </label>
-        {errors.confirmPassword && (
-          <p>{errors.confirmPassword}</p>
-        )}
-        <button type="submit" className="signup-button">Sign Up</button>
-      </form>
-    </div>    
-      
+      <div className="entire-signup-modal">
+        <h1 className="signup-title">Sign Up</h1>
+        <form className="signup-form" onSubmit={handleSubmit}>
+          <label>
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              placeholder="Email"
+              className="signup-input-field"
+            />
+          </label>
+          {errors.email && <p>{errors.email}</p>}
+          <label>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+              placeholder="Username"
+              className="signup-input-field"
+            />
+          </label>
+          {errors.username && <p>{errors.username}</p>}
+          <label>
+            <input
+              type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              required
+              placeholder="First Name"
+              className="signup-input-field"
+            />
+          </label>
+          {errors.firstName && <p>{errors.firstName}</p>}
+          <label>
+            <input
+              type="text"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              required
+              placeholder="Last Name"
+              className="signup-input-field"
+            />
+          </label>
+          {errors.lastName && <p>{errors.lastName}</p>}
+          <label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              placeholder="Password"
+              className="signup-input-field"
+            />
+          </label>
+          {errors.password && <p>{errors.password}</p>}
+          <label>
+            <input
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+              placeholder="Confirm Password"
+              className="signup-input-field"
+            />
+          </label>
+          {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
+          <button type="submit" className="signup-button" disabled={isInvalid}>
+            Sign Up
+          </button>
+        </form>
+      </div>
     </>
   );
 }
--- frontend/src/components/SpotReviews/ReviewForSpot.js
+++ frontend/src/components/SpotReviews/ReviewForSpot.js
@@ -1,3 +1,116 @@
+// import { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { useParams } from "react-router-dom";
+// import { thunkLoadReviewsForSpot } from "../../store/reviewReducer";
+// import { useModal } from "../../context/Modal";
+// import CreateReviewForSpots from "./CreateReviewForSpot";
+// import OpenModalButton from "../OpenModalButton";
+// import DeleteReview from "./DeleteReview";
+// import "./ReviewForSpot.css";
+
+// const ReviewsForSpot = ({ spotData }) => {
+//   const dispatch = useDispatch();
+//   const { spotId } = useParams();
+//   const spotDataId = spotData.id;
+//   console.log("spotData", spotData);
+
+//   useEffect(() => {
+//     dispatch(thunkLoadReviewsForSpot(spotId));
+//   }, [dispatch, spotId]);
+
+//   const revObj = useSelector((state) => state);
+//   const reviews = revObj.review;
+//   const reviewsArr = Object.values(reviews);
+//   const sessionUserId = revObj.session.user?.id; // Use optional chaining to access user id
+//   const user = revObj.session.user;
+//   const ownerId = revObj.spot[spotDataId].ownerId;
+//   let userLoggedIn = null;
+
+//   // Check if the user is logged in and not the owner of the spot
+//   if (sessionUserId && sessionUserId !== ownerId && Object.keys(reviews).length === 0) {
+//     userLoggedIn = (
+//       <div className="review-modal-button">
+//         <OpenModalButton
+//           className="review-button"
+//           buttonText="Post a review"
+//           modalComponent={
+//             <CreateReviewForSpots spotId={spotDataId} sessionUser={user} />
+//           }
+//         />
+//       </div>
+//     );
+//   }
+
+//   if (spotData.avgStar === null) {
+//     spotData.avgStar = "New";
+//   }
+ 
+//   return (
+//     <>
+//       <div className="whole-reviews-container">
+//         <div className="description-for-reviews">
+//           <i className="fa fa-star fa-s"></i>
+//           <h3 className="avg-star-rating">{!isNaN(spotData.avgStar) ? spotData.avgStar.toFixed(2) : "New"}</h3>
+//           <div>&#x2022;</div>
+//           <h3 className="number-review-header">{`${spotData?.numReviews} ${
+//             spotData?.numReviews > 1 ? "reviews" : "review"
+//           }`}</h3>
+//           {userLoggedIn}
+//         </div>
+//         <div className="lower-section-container">
+//           {reviewsArr.length > 0 ? (
+//             reviewsArr.map((review) => (
+//               <div key={review?.id} className="review-and-pic">
+//                 <div className="review-with-delete">
+//                   <div className="pic-name-date-div">
+//                     <i className="fas fa-user fa-2x" />
+//                     <div className="name-date-buttons-container">
+//                       <div className="name-and-date-div">
+//                         <div className="review-user-firstname">{review?.User?.firstName}</div>
+//                         <div className="review-date">
+//                           {new Date(review?.createdAt).toLocaleString("en-US", {
+//                             month: "long",
+//                           })}{" "}
+//                           {new Date(review.createdAt).getFullYear()}
+//                         </div>
+//                       </div>
+//                       <div className="edit-review-div-container">
+//                         {sessionUserId && sessionUserId === review.userId ? (
+//                           <OpenModalButton
+//                             className="delete-review"
+//                             buttonText="Delete"
+//                             modalComponent={<DeleteReview review={review} sessionUser={user} />}
+//                           />
+//                         ) : null}
+//                       </div>
+//                     </div>
+//                   </div>
+//                   <div className="review-by-user">{review?.review}</div>
+//                 </div>
+//               </div>
+//             ))
+//           ) : (
+//             <div className="no-reviews">
+//             {userLoggedIn
+//               ? "Be the first to post a review!"
+//               : "No reviews available"}
+//           </div>
+//           )}
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+
+
+
+
+
+// export default ReviewsForSpot
+
+
+
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
@@ -26,8 +139,9 @@ const ReviewsForSpot = ({ spotData }) => {
   const ownerId = revObj.spot[spotDataId].ownerId;
   let userLoggedIn = null;
 
-  // Check if the user is logged in and not the owner of the spot
-  if (sessionUserId && sessionUserId !== ownerId && Object.keys(reviews).length === 0) {
+  // Check if the user is logged in, not the owner of the spot, and has not reviewed the spot
+  const userHasReviewed = reviewsArr.some(review => review.User.id === sessionUserId);
+  if (sessionUserId && sessionUserId !== ownerId && !userHasReviewed) {
     userLoggedIn = (
       <div className="review-modal-button">
         <OpenModalButton
@@ -44,7 +158,7 @@ const ReviewsForSpot = ({ spotData }) => {
   if (spotData.avgStar === null) {
     spotData.avgStar = "New";
   }
- 
+
   return (
     <>
       <div className="whole-reviews-container">
@@ -58,8 +172,9 @@ const ReviewsForSpot = ({ spotData }) => {
           {userLoggedIn}
         </div>
         <div className="lower-section-container">
+          
           {reviewsArr.length > 0 ? (
-            reviewsArr.map((review) => (
+            reviewsArr.map((review) => ( 
               <div key={review?.id} className="review-and-pic">
                 <div className="review-with-delete">
                   <div className="pic-name-date-div">
@@ -78,23 +193,27 @@ const ReviewsForSpot = ({ spotData }) => {
                         {sessionUserId && sessionUserId === review.userId ? (
                           <OpenModalButton
                             className="delete-review"
-                            buttonText="Delete"
-                            modalComponent={<DeleteReview review={review} sessionUser={user} />}
+                            buttonText="Delete Review"
+                            modalComponent={<DeleteReview reviewId={review.id} />}
                           />
                         ) : null}
                       </div>
                     </div>
                   </div>
-                  <div className="review-by-user">{review?.review}</div>
+                  <div className="review-body-container">
+                    <div className="stars-user-rating">
+                      <i className="fa fa-star" />
+                      <h3>{review?.stars}</h3>
+                    </div>
+                    <div className="review-body-text">{review?.review}</div>
+                  </div>
                 </div>
               </div>
             ))
           ) : (
-            <div className="no-reviews">
-            {userLoggedIn
-              ? "Be the first to post a review!"
-              : "No reviews available"}
-          </div>
+            <div className="no-review-message">
+              No reviews yet. Be the first one to write a review!
+            </div>
           )}
         </div>
       </div>
@@ -102,6 +221,4 @@ const ReviewsForSpot = ({ spotData }) => {
   );
 };
 
-
-
-export default ReviewsForSpot
+export default ReviewsForSpot;
